2c2650503298e8006e39be61f98e4ff5fb69acaa
Steven Johnson <stevenjohnson86@gmail.com>
2017-09-16 12:46:43 -0700

    added empty android project. added sample twitter json

---
+++ TwitterClient-Android/app/src/main/java/sjohnsoncf/twitterclient_android/HomeTimelineActivity.java
@@ -0,0 +1,13 @@
+package sjohnsoncf.twitterclient_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeTimelineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_timeline);
+    }
+}
